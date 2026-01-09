68f72d10936364a8a2a105fa98331bd256983f5a
Alexis <dyslex1kproductions@gmail.com>
2026-01-09 15:23:47 +0000

    Added unsaved files

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true, // Automatically provides globals like expect
-    environment: 'jsdom', // Set the environment to jsdom (for DOM testing)
-    setupFiles: ['./src/setupTests.ts'], // Optional: for setup files like jest-dom matchers
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
   },
 })
